23620348f0f1e0c3d1ed267a9163a585d73566ab
ankita200328 <shreekrishna280105@gamil.com>
2026-01-13 23:10:28 +0530

    second commit

--- string.js
+++ string.js
@@ -1,5 +1,16 @@
 
 
-let sen="java is fun";
-let reversedSen=sen.split(" ").reverse().join(" ");
-console.log(reversedSen);
+// let sen="java is fun";
+// let reversedSen=sen.split(" ").reverse().join(" ");
+// console.log(reversedSen);
+
+
+function isRotation(s1,s2) {
+    if (s1.lenght !==s2.length) {
+        return false;
+    }
+    return (s1+s2).includes(s2);
+}
+let s1="ABCD";
+let s2="CDAB";
+console.log(isRotation(s1,s2));
